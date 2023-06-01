9349355d567e6d919922250d7476e700334f305e
rifathnehleenreza <nehleen11@gmail.com>
2023-06-01 18:54:34 -0400

    commit to git

---
+++ Day_1_commands.R
@@ -0,0 +1,10 @@
+# assigning values to variable: <-, =
+# class, gives you the idea of the nature of the data
+# names, assigning the elements of a vector, matrix into another variable/group
+# cbind, c() or : means concatenate
+# How to call a variable
+# How to call a element from a vector, matrix etc.
+# selecting sth element of a variable using []
+# Some function: sum(), mean(), these are the base function which are builtin with R
+# matrix creation, matrix <- , how to sort the values of a matrix by row or by column
+# 
---
+++ Day_1_note.R
@@ -0,0 +1,45 @@
+## Shohan Bhai
+## Day: 1
+## 5/26/2023/Friday
+
+## Arithmatic operation
+
+# Addition:
+
+2 + 2
+
+# Subtraction:
+
+2 - 2
+
+# Multiplication:
+
+2 * 2
+
+# Division:
+
+2/2
+
+# Conclusion: R can mimic the functionality of a calculator that-
+# Any arithmathical operations can be done by R in numerical data type.
+
+# Values and Variables
+# Do you know how to assign values to a variable?
+# Now we will consider two signs = or <- they are the tools to assign values to a variable.
+# While coading == (double quote) has a special meaning. To avoid that confusion we prefer using <- sign always
+# Data and Data type or nature of the data or what kind of data is stored inside the variable?
+# When we assign some value to a variable it becomes a data and it is stored in the environment.
+# If we want to know what type of values are stored inside a data we will use a function called 
+# [class]
+# when we try to determine the type of data that are stored under a variable we use the function class,
+# A variable can store 3 types of data - 1. numerical, 2. charecter [assignment should be using ""; double quote], 3. boolean [True/False]
+# There are different ways of storing information into a variable, based on the complexity of the stored data within a variables, we can classify the variables.
+# The variables can be classified as - vector, object, matrix, list etc.
+# IF WE TRY TO CLASSIFY VARIABLES [data storage system/dataframe] => we have object, scaler, vector, matrix, list etc.
+# How do we name a vector?
+# Vector => 1 dimensional
+# Matrix => 2 dimensional
+# Error => the command was not run
+# Error Massege => the command was run but it was not properly run something went wrong
+# Limitation of matrix is they work with same datatype either all numerical or all character or all boolean if not they convert all to same type. eg.- all numerical data are converted to character for a single character element
+# Where dataframe can store different datatypes but it becomes challenging when your are handeling big data.
